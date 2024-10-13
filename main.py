ffcefa84c6c05c8c5607f89ca764d153be75e7d8
Auston Mtabane <mtabane.auston@gmail.com>
2024-10-13 18:07:50 +0200

    added functionality to the function

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
+import os,random
+
 def draw_graph(data):
-    pass
+    m = max(data)
+    sp = 0
+    for y in range(m+1,-1,-1):
+        if y < 10: sp = 1
+        print(" "*sp + str(y),end="")
+        for x in range(len(data)):
+            if data[x] == y:
+                print("*",end="")
+            else:
+                print(" ",end="")
+            
+        print("")
+        
+
+os.system('cls' if os.name == 'nt' else 'clear')
+
+
+draw_graph([1,3,9,8,7])
